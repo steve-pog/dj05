91f24abb82553018159c982be1ef982d093e193f
steve <stevelovesroblox@gmail.com>
2021-02-20 17:11:27 -0500

    h

--- pog/pog/urls.py
+++ pog/pog/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pog2 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.myhouse, name="home")
 ]
--- pog/pog2/views.py
+++ pog/pog2/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def myhouse(request):
+    info ={"key": "value of dictionary"}
+    return render(request, 'anything.html', info)
